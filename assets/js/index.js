c509af4cbe7331461559fe26dff746e033224314
guixing <307688583@qq.com>
2020-11-01 20:02:12 +0800

    登录页主页完成，开始处理个人详情页

---
+++ assets/js/index.js
@@ -0,0 +1,54 @@
+$(function() {
+    //主页打开之后首先调用getUserInfo()函数，获取用户基本信息
+    getUserInfo()
+    // 点击按钮，实现退出功能
+    $('#btnLogout').on('click', function() {
+        // 提示用户是否确认退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function(index) {
+            //do something
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token')
+            // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+
+            // 关闭 confirm 询问框
+            layer.close(index)
+        })
+    })
+})
+// 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+            // 调用 renderAvatar 渲染用户的头像
+            renderAvatar(res.data)
+        }
+    })
+}
+
+// 渲染用户的头像
+function renderAvatar(user) {
+    // 1. 获取用户的名称
+    var name = user.nickname || user.username
+    // 2. 设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    // 3. 按需渲染用户的头像
+    if (user.user_pic !== null) {
+        // 3.1 如果用户自己有个人头像，渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        // 3.2 若用户没有自己的个人头像，渲染文本头像 
+        // 先将系统自带的图片隐藏
+        $('.layui-nav-img').hide()
+        //如果用户名称如果是英文的话，提取第一个字符串转换为大写格式
+        var first = name[0].toUpperCase()
+        // 将转换好的文本内容渲染出来
+        $('.text-avatar').html(first).show()
+    }
+}
